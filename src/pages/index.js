34eaecdd4ea04552a418659c73e09a3b1e9a422e
crossz <ukcrossa@gmail.com>
2022-05-15 12:42:14 +0800

    :tada: Layout added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 // import ReactDOM from "react-dom";
 // import { BrowserRouter } from "react-router-dom";
 // import { Auth0ProviderWithHistory } from "../auth0-provider-with-history";
 // import { EnvProvider } from "../context/env.context";
 // import { Router } from "@reach/router";
-import { Footer } from "../components/footer";
-import { Loader } from "../components/loader";
-import { NavBar } from "../components/nav-bar";
+import Layout from "../components/layout";
+// import { Footer } from "../components/footer";
+// import { Loader } from "../components/loader";
+// import { NavBar } from "../components/nav-bar";
 // import { ProtectedRoute } from "./components/protected-route";
 // import ExternalApi from "./pages/external-api";
 // import NotFound from "./pages/404";
@@ -27,24 +28,10 @@ import "../styles/styles.css";
 // }
 
 const Index = () => {
-  const { isLoading } = useAuth0();
-
-  if (isLoading) {
-    return (
-      <div className="page-layout">
-        <Loader />
-      </div>
-    );
-  }
-
   return (
-    <div className="page-layout">
-      <NavBar />
-      <div className="page-layout__content">
-        <Home />
-      </div>
-      <Footer />
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 };
 
